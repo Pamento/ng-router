46ed068c5167494d17c7f1390922136f0a01c0fd
Jean <demeljean@hotmail.fr>
2018-04-17 11:08:11 +0200

    added templating

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,5 +6,14 @@ import { Component } from "@angular/core";
     templateUrl:'./first.component.html'
 })
 export class FirstComponent {
+    idP:string = 'monId';
+    maVariable:string = 'bloup';
+    showSection:boolean = true;
+    tableau:string[] = ['ga', 'zo', 'bu', 'meu'];
 
+    methode():void {
+        console.log('bloup');
+        this.maVariable = 'blip';
+        this.showSection = !this.showSection;
+    }
 }
